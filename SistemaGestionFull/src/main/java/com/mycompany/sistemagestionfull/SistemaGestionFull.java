e73c06ad4ac302f0eecd97ab6344201ab80e5995
Jesus Puentes <jopt98@gmail.com>
2024-06-13 14:00:50 -0600

    Extra files

---
+++ SistemaGestionFull/src/main/java/com/mycompany/sistemagestionfull/SistemaGestionFull.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.sistemagestionfull;
+
+/**
+ *
+ * @author Lenovo
+ */
+public class SistemaGestionFull {
+
+    public static void main(String[] args) {
+        ClientsForm form = new ClientsForm();
+        form.show();
+    }
+}
